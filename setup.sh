da9d19d0412145efea63c0548f6ceb61055d614d
tomseimandi <tom.seimandi@gmail.com>
2023-01-18 08:52:16 +0000

    Add retrieving service account credentials

--- setup.sh
+++ setup.sh
@@ -2,3 +2,7 @@
 git config --global credential.helper store
 
 pip install -r requirements.txt
+
+AWS_ACCESS_KEY_ID=`vault kv get -field=ACCESS_KEY_ID onyxia-kv/projet-slums-detection/s3` && export AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_ACCESS_KEY onyxia-kv/projet-slums-detection/s3` && export AWS_SECRET_ACCESS_KEY
+export MC_HOST_s3=https://$AWS_ACCESS_KEY_ID:$AWS_SECRET_ACCESS_KEY@$AWS_S3_ENDPOINT
