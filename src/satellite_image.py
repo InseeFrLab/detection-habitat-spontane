33e8bbff10d2cbd77cf3519239396ef4a9b9d521
cguillo <clement.guillo@insee.fr>
2023-03-10 13:29:27 +0000

    ajout d'une fonction de représentation d'une liste image sous forme de matrice pertinente et modification de la classe Satellite image pour que la normalisation laisse inteact les données originales

--- src/satellite_image.py
+++ src/satellite_image.py
@@ -45,6 +45,7 @@ class SatelliteImage:
             normalized (bool): _description_. Defaults to False.
         """
         self.array = array
+        self.array_to_plot = array
         self.crs = crs
         self.bounds = bounds
         self.transform = transform
@@ -84,10 +85,14 @@ class SatelliteImage:
                 filename=self.filename,
                 dep=self.dep,
                 date=self.date,
-                normalized=self.normalized,
+                normalized=False,
             )
             for rows, cols in indices
         ]
+        
+        for im, indice in zip(splitted_images,indices):
+            im.array_to_plot = self.array_to_plot[:, indice[0][0] : indice[0][1], indice[1][0] : indice[1][1]]
+            
         return splitted_images
 
         raise NotImplementedError()
@@ -125,7 +130,7 @@ class SatelliteImage:
             )
             for i in range(self.n_bands)
         ]
-        self.array = np.stack(normalized_bands)
+        self.array_to_plot = np.stack(normalized_bands)
         self.normalized = True
 
     def plot(self, bands_indices: List):
@@ -140,12 +145,66 @@ class SatelliteImage:
             self.normalize()
 
         fig, ax = plt.subplots(figsize=(5, 5))
-        ax.imshow(np.transpose(self.array, (1, 2, 0))[:, :, bands_indices])
+        ax.imshow(np.transpose(self.array_to_plot, (1, 2, 0))[:, :, bands_indices])
         plt.xticks([])
         plt.yticks([])
-        plt.title(f"Dimension of image {self.array.shape[1:]}")
+        plt.title(f"Dimension of image {self.array_to_plot.shape[1:]}")
+        plt.show()
+    
+    @staticmethod
+    def plot_list_satellite_images(list_images: List,bands_indices: List):
+        """Plot a list of SatelliteImage (with a subset of bands) into a grid following the coordinates of the SatelliteImage
+
+        Args:
+            list_images (List): List of SatelliteImage objects
+            bands_indices (List): List of indices of bands to plot.
+                The indices should be integers between 0 and the
+                number of bands - 1.
+        """
+        list_bounding_box = np.array([im.bounds for im in list_images]) 
+        list_images = np.array(list_images)
+        Y = np.array([bb[0] for bb in list_bounding_box])
+        order_y = np.argsort(np.array(Y))
+        Y = Y[order_y]
+
+        list_images = list_images[order_y]
+        list_bounding_box = list_bounding_box[order_y]
+
+        X = np.array([bb[3] for bb in list_bounding_box])
+        list_images = list_images[np.lexsort((Y,X))]
+
+        n_col = len(np.unique(np.array([bb[0] for bb in list_bounding_box])))
+        n_row = len(np.unique(np.array([bb[3] for bb in list_bounding_box])))
+
+        mat_list_images = np.transpose(list_images.reshape(n_col,n_row))
+
+        # Create the grid of pictures and fill it
+        images = np.empty((n_col,n_row), dtype = object)
+
+        for i in range(n_col):
+            for j in range(n_row):
+                images[i,j] = mat_list_images[i,j].array_to_plot
+
+        images =np.flip(np.transpose(images),axis=0)
+
+        # Create a figure and axes
+        fig, axs = plt.subplots(nrows=n_col, ncols=n_row, figsize=(10, 10))
+
+        # Iterate over the grid of  images and plot them
+        for i in range(n_col):
+            for j in range(n_row):
+                axs[i,j].imshow(np.transpose(images[i,j], (1, 2, 0))[:, :, bands_indices])
+
+        # Remove any unused axes
+        for i in range(n_col):
+            for j in range(n_row):
+                axs[i,j].set_axis_off()
+
+        # Show the plot
         plt.show()
 
+    
+    
     @staticmethod
     def from_raster(
         file_path: str,
