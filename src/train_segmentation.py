94085b090dc7a1641130c3dc8681e1582327a4c7
cguillo <clement.guillo@insee.fr>
2023-04-17 19:53:46 +0000

    documentation et correction flake 8

--- src/train_segmentation.py
+++ src/train_segmentation.py
@@ -1,30 +1,33 @@
-import sys
-sys.path.append('../src')
-from datetime import datetime
-import numpy as np
-import os
+"""
+Script to train a DL model.
+"""
 import gc
-
-from utils.gestion_data import load_pleiade_data, write_splitted_images_masks, build_dataset_train, build_dataset_test, instantiate_module
-from utils.utils import update_storage_access
-from utils.labeler import RILLabeler
-from datas.components.dataset import PleiadeDataset
+import os
+import sys
+from datetime import datetime
 
 import albumentations as album
-from albumentations.pytorch.transforms import ToTensorV2
-import torch
-
+import mlflow
 import pytorch_lightning as pl
+import torch
+from albumentations.pytorch.transforms import ToTensorV2
 from pytorch_lightning.callbacks import (
     EarlyStopping,
     LearningRateMonitor,
     ModelCheckpoint,
 )
 
-from models.segmentation_module import SegmentationModule
+from datas.components.dataset import PleiadeDataset
 from datas.datamodule import DataModule
-
-import mlflow
+from models.segmentation_module import SegmentationModule
+from utils.gestion_data import (
+    build_dataset_test,
+    build_dataset_train,
+    instantiate_module,
+    load_pleiade_data,
+)
+from utils.labeler import RILLabeler
+from utils.utils import update_storage_access
 
 
 def main(remote_server_uri, experiment_name, run_name):
@@ -32,34 +35,34 @@ def main(remote_server_uri, experiment_name, run_name):
     Main function.
     """
 
-    config = { 
-        "tile size" : 250,
+    config = {
+        "tile size": 250,
         "source train": "PLEIADE",
-        "type labeler" : "RIL", # None if source train != PLEIADE
-        "buffer size" : 10, # None if BDTOPO
-        "year"  : 2022,
-        "territory" : "martinique",
-        "dep" : "972",
-        "n bands" : 3
+        "type labeler": "RIL",  # None if source train != PLEIADE
+        "buffer size": 10,  # None if BDTOPO
+        "year": 2022,
+        "territory": "martinique",
+        "dep": "972",
+        "n bands": 3,
     }
 
-    config_train = { 
+    config_train = {
         "lr": 0.0001,
         "momentum": 0.9,
-        "module" : "deeplabv3",
-        "batch size" : 9,
-        "max epochs" : 100
+        "module": "deeplabv3",
+        "batch size": 9,
+        "max epochs": 100,
     }
 
-    # params 
+    # params
     tile_size = config["tile size"]
     n_bands = config["n bands"]
     dep = config["dep"]
     territory = config["territory"]
     year = config["year"]
-    buffer_size = config["buffer size"] 
-    source_train = config["source train"] 
-    type_labeler = config["type labeler"] 
+    buffer_size = config["buffer size"]
+    source_train = config["source train"]
+    type_labeler = config["type labeler"]
 
     module = config_train["module"]
     batch_size = config_train["batch size"]
@@ -68,46 +71,63 @@ def main(remote_server_uri, experiment_name, run_name):
 
     update_storage_access()
     os.environ["MLFLOW_S3_ENDPOINT_URL"] = "https://minio.lab.sspcloud.fr"
-    #%env MLFLOW_S3_ENDPOINT_URL=https://minio.lab.sspcloud.fr
+    # %env MLFLOW_S3_ENDPOINT_URL=https://minio.lab.sspcloud.fr
     # DL des données du teritoire dont on se sert pour l'entraînement
 
     if source_train == "PLEIADE":
-
         # Plus tard décliner avec change detection etc..
         if type_labeler == "RIL":
-            date = datetime.strptime(str(year)+"0101",'%Y%m%d')
-            labeler = RILLabeler(date, dep = dep, buffer_size = buffer_size)     
+            date = datetime.strptime(str(year) + "0101", "%Y%m%d")
+            labeler = RILLabeler(date, dep=dep, buffer_size=buffer_size)
 
-        dataset_train =  build_dataset_train(year,territory,dep,labeler,tile_size,n_bands,train_directory_name,PleiadeDataset)
-        load_pleiade_data(2020,"mayotte")
-
-        dataset_test = build_dataset_test("../data/PLEIADES/2020/MAYOTTE/ORT_2020052526670967_0519_8586_U38S_8Bits.jp2",3,250,labeler,PleiadeDataset)
-        image_size = (tile_size,tile_size)
-
-    transforms_augmentation = album.Compose(
-            [
-                album.Resize(300, 300, always_apply=True),
-                album.RandomResizedCrop(
-                    *image_size, scale=(0.7, 1.0), ratio=(0.7, 1)
-                ),
-                album.HorizontalFlip(),
-                album.VerticalFlip(),
-                album.Normalize(),
-                ToTensorV2(),
-           ]
+        dataset_train = build_dataset_train(
+            year,
+            territory,
+            dep,
+            labeler,
+            tile_size,
+            n_bands,
+            train_directory_name,
+            PleiadeDataset,
         )
 
-    transforms_preprocessing = album.Compose(
-            [
-                album.Resize(*image_size, always_apply=True),
-                album.Normalize(),
-                ToTensorV2(),
-            ]
+        load_pleiade_data(2020, "mayotte")
+
+        test_file = "../data/PLEIADES/2020/MAYOTTE, \
+            ORT_2020052526670967_0519_8586_U38S_8Bits.jp2"
+
+        dataset_test = build_dataset_test(
+            test_file, 3, 250, labeler, PleiadeDataset
+        )
+        image_size = (tile_size, tile_size)
+
+    transforms_augmentation = album.Compose(
+        [
+            album.Resize(300, 300, always_apply=True),
+            album.RandomResizedCrop(
+                *image_size, scale=(0.7, 1.0), ratio=(0.7, 1)
+            ),
+            album.HorizontalFlip(),
+            album.VerticalFlip(),
+            album.Normalize(),
+            ToTensorV2(),
+        ]
     )
 
-    ## Instanciation modèle et paramètres d'entraînement
+    transforms_preprocessing = album.Compose(
+        [
+            album.Resize(*image_size, always_apply=True),
+            album.Normalize(),
+            ToTensorV2(),
+        ]
+    )
+
+    # Instanciation modèle et paramètres d'entraînement
     optimizer = torch.optim.SGD
-    optimizer_params = {"lr": config_train["lr"], "momentum":  config_train["momentum"]}
+    optimizer_params = {
+        "lr": config_train["lr"],
+        "momentum": config_train["momentum"],
+    }
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau
     scheduler_params = {}
     scheduler_interval = "epoch"
@@ -115,14 +135,13 @@ def main(remote_server_uri, experiment_name, run_name):
     model = instantiate_module(module)
 
     data_module = DataModule(
-        dataset= dataset_train,
+        dataset=dataset_train,
         transforms_augmentation=transforms_augmentation,
         transforms_preprocessing=transforms_preprocessing,
-        num_workers=1, 
-        batch_size= batch_size,
-        dataset_test = dataset_test
+        num_workers=1,
+        batch_size=batch_size,
+        dataset_test=dataset_test,
     )
-
 
     lightning_module = SegmentationModule(
         model=model,
@@ -132,7 +151,6 @@ def main(remote_server_uri, experiment_name, run_name):
         scheduler_params=scheduler_params,
         scheduler_interval=scheduler_interval,
     )
-
 
     checkpoint_callback = ModelCheckpoint(
         monitor="validation_IOU", save_top_k=1, save_last=True, mode="max"
@@ -144,10 +162,10 @@ def main(remote_server_uri, experiment_name, run_name):
 
     lr_monitor = LearningRateMonitor(logging_interval="step")
 
-    strategy ="auto"
+    strategy = "auto"
     list_callbacks = [lr_monitor, checkpoint_callback, early_stop_callback]
 
-    #!pip install mlflow
+    # !pip install mlflow
     mlflow.end_run()
 
     mlflow.set_tracking_uri(remote_server_uri)
@@ -158,27 +176,27 @@ def main(remote_server_uri, experiment_name, run_name):
     gc.collect()
 
     with mlflow.start_run(run_name=run_name):
-
         trainer = pl.Trainer(
-        callbacks= list_callbacks,
-        max_epochs=config_train["max epochs"],
-        num_sanity_val_steps=2,
-        strategy=strategy,
-        log_every_n_steps=2
+            callbacks=list_callbacks,
+            max_epochs=config_train["max epochs"],
+            num_sanity_val_steps=2,
+            strategy=strategy,
+            log_every_n_steps=2,
         )
 
         trainer.fit(lightning_module, datamodule=data_module)
 
         print("test")
-        trainer.test(lightning_module , datamodule= data_module)
-    
-    
+        trainer.test(lightning_module, datamodule=data_module)
+
+
 if __name__ == "__main__":
     # MLFlow params
     remote_server_uri = sys.argv[1]
     experiment_name = sys.argv[2]
     run_name = sys.argv[3]
-    
+
     main(remote_server_uri, experiment_name, run_name)
-    
-#python train_segmentation.py https://projet-slums-detection-386760.user.lab.sspcloud.fr segmentation bibi
+
+# python train_segmentation.py \
+# https://projet-slums-detection-386760.user.lab.sspcloud.fr segmentation bibi
--- src/utils/change_detection_triplet.py
+++ src/utils/change_detection_triplet.py
@@ -1,7 +1,6 @@
 """
 """
-from datetime import datetime
-from typing import List, Literal, Tuple
+from typing import List
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -10,29 +9,30 @@ from PIL import Image
 from utils.satellite_image import SatelliteImage
 from utils.utils import get_indices_from_tile_length
 
- 
+
 class ChangedetectionTripletS2Looking:
     """ """
+
     def __init__(
         self,
         pathimage1: str,
         pathimage2: str,
         pathlabel: str,
     ):
-    
         """
         Constructor.
 
         Args:
-            satellite_image (SatelliteImage): Satellite Image.
-           
+            pathimage1 (str): Path to the first image.
+            pathimage2 (str): Path to the second image.
+            pathlabel (str): Path to the label image.
+
         """
         self.image1 = Image.open(pathimage1)
         self.image2 = Image.open(pathimage2)
-        self.label  = Image.open(pathlabel)
-        
+        self.label = Image.open(pathlabel)
+
     def plot(self):
-        
         fig, axes = plt.subplots(ncols=3, figsize=(10, 20))
 
         # Plot each image on a separate subplot
@@ -46,57 +46,51 @@ class ChangedetectionTripletS2Looking:
 
         # Show the plot
         plt.show()
-    
-    def random_crop(self,tile_size):
-        
+
+    def random_crop(self, tile_size):
         width = self.image1.width
         height = self.image2.height
 
-        num_subparts_x = width//tile_size 
-        num_subparts_y =  height//tile_size 
+        num_subparts_x = width // tile_size
+        num_subparts_y = height // tile_size
 
         # sélection aléatoire d'une aprtie de l'image pour le dataset
         i = np.random.randint(num_subparts_x)
         j = np.random.randint(num_subparts_y)
 
         left = j * tile_size
-        right = (j+1) * tile_size
+        right = (j + 1) * tile_size
         top = i * tile_size
-        bottom =(i+1)*tile_size
-        
-        self.image1 = self.image1.crop((left,top,right,bottom))
-        self.image2 = self.image2.crop((left,top,right,bottom))
-        self.label  = self.label.crop((left,top,right,bottom))
-        
+        bottom = (i + 1) * tile_size
 
+        self.image1 = self.image1.crop((left, top, right, bottom))
+        self.image2 = self.image2.crop((left, top, right, bottom))
+        self.label = self.label.crop((left, top, right, bottom))
 
 
 class ChangeDetectionTriplet:
     """ """
+
     def __init__(
         self,
         satellite_image1: SatelliteImage,
         satellite_image2: SatelliteImage,
-        label: np.array,
-        source: Literal["RIL", "BDTOPO"],
-        labeling_date1: datetime,
-        labeling_date2: datetime
+        label: np.array,  # shall contain the difference mask
     ):
         """
         Constructor.
 
         Args:
-            satellite_image (SatelliteImage): Satellite Image.
+            satellite_image1 (SatelliteImage): Satellite Image.
+            satellite_image2 (SatelliteImage): Satellite Image.
             label (np.array): Building change segmentation mask.
-            source (Literal["RIL", "BDTOPO"]): Labeling source.
-            labeling_date (datetime): Date of labeling data.
-        """
-        self.satellite_image = satellite_image
-        self.label = label
-        self.source = source
-        self.labeling_date = labeling_date
 
-    def split(self, tile_length: int ) -> List:
+        """
+        self.satellite_image1 = satellite_image1
+        self.satellite_image2 = satellite_image2
+        self.label = label
+
+    def split(self, tile_length: int) -> List:
         """
         Split the SegmentationLabeledSatelliteImage into tiles of
         dimension (`tile_length` x `tile_length`).
@@ -105,12 +99,12 @@ class ChangeDetectionTriplet:
             tile_length (int): Dimension of tiles
 
         Returns:
-            List[SegmentationLabeledSatelliteImage]: _description_
+            List[ChangeDetectionTriplet]: _description_
         """
         # 1) on split la liste de satellite image avec la fonction déjà codée
         list_sat1 = self.satellite_image1.split(tile_length=tile_length)
         list_sat2 = self.satellite_image2.split(tile_length=tile_length)
-        
+
         # 2) on split le masque
         if tile_length % 2:
             raise ValueError("Tile length has to be an even number.")
@@ -126,36 +120,34 @@ class ChangeDetectionTriplet:
 
         list_cd_triplet = [
             ChangeDetectionTriplet(
-                im1,im2, label, self.source, self.labeling_date
+                im1, im2, label, self.source, self.labeling_date
             )
-            for im, label in zip(list_sat1, list_sat2, splitted_labels)
+            for im1, im2, label in zip(list_sat1, list_sat2, splitted_labels)
         ]
-        
+
         return list_cd_triplet
 
     def plot(self, bands_indices: List, alpha=0.3):
         """
-        Plot a subset of bands from a satellite image and its
+        Plot a subset of bands from a change detection satellite image and its
         corresponding labels as an image.
 
         Args:
-        bands_indices (List): List of indices of bands to plot from the
-        satellite image. The indices should be integers between 0 and
-        the number of bands - 1.
-        alpha (float, optional): The transparency of the label image when
-        overlaid on the satellite image. A value of 0 means fully transparent
-        and a value of 1 means fully opaque. The default value is 0.3.
+            bands_indices (List): List of indices of bands to plot from \
+            the satellite image.The indices should be integers \
+            between 0 and the number of bands - 1.
+            alpha (float, optional): The transparency of the label image when \
+            overlaid on the satellite image. The default value is 0.3.
 
         """
 
         if not self.satellite_image1.normalized:
             self.satellite_image1.normalize()
-        
-        
+
         if not self.satellite_image2.normalized:
             self.satellite_image2.normalize()
-            
-        fig, (ax1, ax2, ax3) = plt.subplots(ncols = 3, figsize=(10, 20))
+
+        fig, (ax1, ax2, ax3) = plt.subplots(ncols=3, figsize=(10, 20))
         ax1.imshow(
             np.transpose(self.satellite_image1.array, (1, 2, 0))[
                 :, :, bands_indices
@@ -171,8 +163,3 @@ class ChangeDetectionTriplet:
         plt.yticks([])
         plt.title(f"Dimension of image {self.satellite_image.array.shape[1:]}")
         plt.show()
-    
-    
-    
-    
-    
