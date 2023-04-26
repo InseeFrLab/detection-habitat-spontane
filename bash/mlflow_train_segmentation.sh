0fea3398c62f4444c80f748ae209bd31f1c8f4f0
Thomas Faria <thomas.faria@ensae.fr>
2023-04-26 11:51:40 +0200

    minor changes in bash file

--- bash/mlflow_train_segmentation.sh
+++ bash/mlflow_train_segmentation.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
-AWS_ACCESS_KEY_ID=`vault kv get -field=ACCESS_KEY_ID onyxia-kv/projet-slums-detection/s3` && export AWS_ACCESS_KEY_ID
-AWS_SECRET_ACCESS_KEY=`vault kv get -field=SECRET_ACCESS_KEY onyxia-kv/projet-slums-detection/s3` && export AWS_SECRET_ACCESS_KEY
-unset AWS_SESSION_TOKEN
+
+# Set MLFLOW_EXPERIMENT_NAME environment variable
+export MLFLOW_EXPERIMENT_NAME="segmentation"
+
+# Set MLFLOW_RUN_NAME environment variable
+export MLFLOW_RUN_NAME="testraya_1"
+
+# Set MLFLOW_EXPERIMENT_NAME environment variable
 export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
 
 # Set MLFLOW_TRACKING_URI environment variable
@@ -17,6 +22,8 @@ while IFS= read -r line; do
 done <<< "$GET_PODS"
 
 export MLFLOW_TRACKING_URI="https://projet-slums-detection-$POD_ID.user.lab.sspcloud.fr"
-export MLFLOW_EXPERIMENT_NAME="segmentation"
 
-mlflow run ~/work/detection-bidonvilles/ --entry-point segmentation --env-manager=local -P remote_server_uri=$MLFLOW_TRACKING_URI
+mlflow run ~/work/detection-bidonvilles/ --entry-point segmentation --env-manager=local \
+-P remote_server_uri=$MLFLOW_TRACKING_URI \
+-P experiment_name=$MLFLOW_EXPERIMENT_NAME \
+-P run_name=$MLFLOW_RUN_NAME
