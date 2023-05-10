34480224052edcf7e2b61473f4f64e387e649453
cguillo <clement.guillo@insee.fr>
2023-05-10 14:38:07 +0000

    corrections de stryle

--- src/classes/optim/evaluation_model.py
+++ src/classes/optim/evaluation_model.py
@@ -1,5 +1,11 @@
 import torch
-
+import numpy as np
+from classes.data.satellite_image import SatelliteImage
+from classes.data.labeled_satellite_image import \
+    SegmentationLabeledSatelliteImage
+from utils.plot_utils import \
+    plot_list_segmentation_labeled_satellite_image
+import os
 # with open("../config.yml") as f:
 #     config = yaml.load(f, Loader=SafeLoader)
 
@@ -10,49 +16,59 @@ import torch
 # train_dl, valid_dl, test_dl = instantiate_dataloader(
 #     config, list_output_dir
 # )
-def evaluer_modele_sur_jeu_de_test_segmentation_pleiade(test_dl, model, tile_size, batch_size, mlflow = False):
+
+
+def evaluer_modele_sur_jeu_de_test_segmentation_pleiade(
+    test_dl,
+    model,
+    tile_size,
+    batch_size,
+    mlflow=False
+):
 
     npatch = int((2000/tile_size)**2)
     nbatchforfullimage = int(npatch/batch_size)
-    
+
     if not npatch % nbatchforfullimage == 0:
-        print("Le nombre de patchs n'est pas divisible par la taille d'un batch")
-        return None 
-    
+        print("Le nombre de patchs \
+            n'est pas divisible par la taille d'un batch")
+        return None
+
     list_labeled_satellite_image = []
 
     for idx, batch in enumerate(test_dl):
-        
+       
         images, label, dic = batch
         output_model = model(images)
-        mask_pred = np.array(torch.argmax(output_model, axis  = 1))
+        mask_pred = np.array(torch.argmax(output_model, axis=1))
 
-        for i in range(batch_size):    
+        for i in range(batch_size):
             pthimg = dic["pathimage"][i]
             si = SatelliteImage.from_raster(
-                file_path = pthimg,
-                dep = None,
-                date = None,
-                n_bands= 3
+                file_path=pthimg,
+                dep=None,
+                date=None,
+                n_bands=3
             )
             si.normalize()
-        
+
             list_labeled_satellite_image.append( 
                 SegmentationLabeledSatelliteImage(
-                    satellite_image =  si ,
-                    label= mask_pred[i],
-                    source= "",
-                    labeling_date = ""
+                    satellite_image=si,
+                    label=mask_pred[i],
+                    source="",
+                    labeling_date=""
                 )
             )
-        
 
         if ((idx+1) % nbatchforfullimage) == 0:
             print("ecriture image")
             if not os.path.exists("img/"):
                 os.makedirs("img/")
 
-            fig1 = plot_list_segmentation_labeled_satellite_image(list_labeled_satellite_image,[0,1,2])
+            fig1 = plot_list_segmentation_labeled_satellite_image(
+                list_labeled_satellite_image, [0, 1, 2]
+                )
     
             filename = pthimg.split('/')[-1]
             filename = filename.split('.')[0]
@@ -67,7 +83,6 @@ def evaluer_modele_sur_jeu_de_test_segmentation_pleiade(test_dl, model, tile_siz
             
         del images, label, dic
     
-
 
 def calculate_IOU(output, labels):
     """
