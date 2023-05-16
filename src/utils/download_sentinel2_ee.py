366068c299333e49c43125afa2344198f8c78323
jnabec <judith.nabec@edu.univ-eiffel.fr>
2023-05-16 15:35:46 +0000

    prise en compte commentaires PR

--- src/utils/download_sentinel2_ee.py
+++ src/utils/download_sentinel2_ee.py
@@ -3,12 +3,10 @@ import shutil
 
 import ee
 import geemap
-import hvac
-import s3fs
-from osgeo import gdal
-from tqdm import tqdm
+from download_sentinel1_ee import upload_satelliteImages
 
-from classes.data.satellite_image import SatelliteImage
+import utils.mappings
+from utils.utils import get_environment, get_root_path, update_storage_access
 
 service_account = (
     "slums-detection-sa@ee-insee-sentinel.iam.gserviceaccount.com"
@@ -154,7 +152,43 @@ def export_s2_no_cloud(
     cld_prj_dist,
     buffer,
 ):
-    AOI = ee.Geometry.BBox(**AOIs[DOM])
+    """
+    Downloads the images locally and calls a function that\
+        uplaods them on MinIO.
+
+    Args:
+        DOM: name of the DOM.
+        AOIs: western, southern, eastern and northern boudaries of the DOMs.
+        EPSGs: EPSGs of the DOMs.
+        start_date: date from which the images can be downloaded.
+        end_date: date after which the images can no longer be downloaded.
+        cloud_filter: maximum image cloud cover percent allowed\
+            in image collection.
+        cloud_prb_thresh: cloud probability (%); values greater than\
+            are considered cloud.
+        nir_drk_thresh: near-infrared reflectance; values less than\
+            are considered potential cloud shadow.
+        cld_prj_dist: maximum distance (km) to search for cloud shadows\
+            from cloud edges.
+        buffer: distance (m) to dilate the edge of cloud-identified objects.
+    """
+
+    update_storage_access()
+    root_path = get_root_path()
+    environment = get_environment()
+
+    bucket = environment["bucket"]
+    path_s3 = environment["sources"]["SENTINEL2"][int(start_date[0:4])][
+        DEPs[DOM.upper()]
+    ]
+    path_local = os.path.join(
+        root_path,
+        environment["local-path"]["SENTINEL2"][int(start_date[0:4])][
+            DEPs[DOM.upper()]
+        ],
+    )
+
+    AOI = ee.Geometry.BBox(**AOIs[DOM.upper()])
     s2_sr_cld_col = get_s2_sr_cld_col(AOI, START_DATE, END_DATE)
     s2_sr_median = (
         s2_sr_cld_col.map(add_cld_shdw_mask).map(apply_cld_shdw_mask).median()
@@ -164,151 +198,35 @@ def export_s2_no_cloud(
     geemap.download_ee_image_tiles(
         s2_sr_median,
         fishnet,
-        f"{DOM}_{start_date[0:4]}/",
+        path_local,
         prefix="data_",
-        crs=f"EPSG:{EPSGs[DOM]}",
+        crs=f"EPSG:{EPSGs[DOM.upper()]}",
         scale=10,
         num_threads=50,
     )
 
     upload_satelliteImages(
-        f"{DOM}_{start_date[0:4]}",
-        f"projet-slums-detection/Donnees/SENTINEL2/{DOM.upper()}/TUILES_{start_date[0:4]}",  # noqa: E501
-        DEPs[DOM],
+        path_local,
+        f"{bucket}/{path_s3}",
+        DEPs[DOM.upper()],
         250,
     )
 
-    shutil.rmtree(f"{DOM}_{start_date[0:4]}", ignore_errors=True)
+    shutil.rmtree(path_local, ignore_errors=True)
 
 
-def exportToMinio(image, rpath):
-    client = hvac.Client(
-        url="https://vault.lab.sspcloud.fr", token=os.environ["VAULT_TOKEN"]
-    )
+if __name__ == "__main__":
+    EPSGs = utils.mappings.name_dep_to_crs
+    DEPs = utils.mappings.name_dep_to_num_dep
+    AOIs = utils.mappings.name_dep_to_aoi
 
-    secret = os.environ["VAULT_MOUNT"] + os.environ["VAULT_TOP_DIR"] + "/s3"
-    mount_point, secret_path = secret.split("/", 1)
-    secret_dict = client.secrets.kv.read_secret_version(
-        path=secret_path, mount_point=mount_point
-    )
-
-    os.environ["AWS_ACCESS_KEY_ID"] = secret_dict["data"]["data"][
-        "ACCESS_KEY_ID"
-    ]
-    os.environ["AWS_SECRET_ACCESS_KEY"] = secret_dict["data"]["data"][
-        "SECRET_ACCESS_KEY"
-    ]
-
-    try:
-        del os.environ["AWS_SESSION_TOKEN"]
-    except KeyError:
-        pass
-
-    fs = s3fs.S3FileSystem(
-        client_kwargs={"endpoint_url": "https://" + "minio.lab.sspcloud.fr"},
-        key=os.environ["AWS_ACCESS_KEY_ID"],
-        secret=os.environ["AWS_SECRET_ACCESS_KEY"],
-    )
-
-    return fs.put(image, rpath, True)
-
-
-def upload_satelliteImages(lpath, rpath, dep, dim):
-    images_paths = os.listdir(lpath)
-
-    for i in range(len(images_paths)):
-        images_paths[i] = lpath + "/" + images_paths[i]
-
-    list_satelliteImages = [
-        SatelliteImage.from_raster(filename, dep=dep, n_bands=12)
-        for filename in tqdm(images_paths)
-    ]
-
-    splitted_list_images = [
-        im
-        for sublist in tqdm(list_satelliteImages)
-        for im in sublist.split(dim)
-    ]
-
-    for i in range(len(splitted_list_images)):
-        image = splitted_list_images[i]
-
-        transf = image.transform
-        in_ds = gdal.Open(images_paths[1])
-        proj = in_ds.GetProjection()
-
-        array = image.array
-
-        driver = gdal.GetDriverByName("GTiff")
-        out_ds = driver.Create(
-            f"image{i}.tif",
-            array.shape[2],
-            array.shape[1],
-            array.shape[0],
-            gdal.GDT_Float64,
-        )
-        out_ds.SetGeoTransform(
-            [transf[2], transf[0], transf[1], transf[5], transf[3], transf[4]]
-        )
-        out_ds.SetProjection(proj)
-
-        for j in range(array.shape[0]):
-            out_ds.GetRasterBand(j + 1).WriteArray(array[j, :, :])
-
-        out_ds = None
-
-        exportToMinio(f"image{i}.tif", rpath)
-        os.remove(f"image{i}.tif")
-
-
-AOIs = {
-    "Guadeloupe": {
-        "west": -61.811124,
-        "south": 15.828534,
-        "east": -60.998518,
-        "north": 16.523944,
-    },
-    "Martinique": {
-        "west": -61.264617,
-        "south": 14.378599,
-        "east": -60.781573,
-        "north": 14.899453,
-    },
-    "Mayotte": {
-        "west": 45.013633,
-        "south": -13.006619,
-        "east": 45.308891,
-        "north": -12.633022,
-    },
-    "Guyane": {
-        "west": -52.883,
-        "south": 4.148,
-        "east": -51.813,
-        "north": 5.426,
-    },
-}
-
-EPSGs = {
-    "Guadeloupe": "4559",
-    "Martinique": "4559",
-    "Mayotte": "4471",
-    "Guyane": "4235",
-}
-
-DEPs = {
-    "Guadeloupe": "971",
-    "Martinique": "972",
-    "Mayotte": "976",
-    "Guyane": "973",
-}
-
-START_DATE = "2021-08-01"
-END_DATE = "2021-09-01"
-CLOUD_FILTER = 60
-CLD_PRB_THRESH = 40
-NIR_DRK_THRESH = 0.15
-CLD_PRJ_DIST = 2
-BUFFER = 50
+    START_DATE = "2022-05-01"
+    END_DATE = "2022-09-01"
+    CLOUD_FILTER = 60
+    CLD_PRB_THRESH = 40
+    NIR_DRK_THRESH = 0.15
+    CLD_PRJ_DIST = 2
+    BUFFER = 50
 
 
 # export_s2_no_cloud(
