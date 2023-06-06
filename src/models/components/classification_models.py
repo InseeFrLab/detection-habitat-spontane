0a63140d2cc8bfa07df0b623c039128103233b67
rayaberova7 <rayaberova7@gmail.com>
2023-06-06 10:17:16 +0000

    modif modèle

--- src/models/components/classification_models.py
+++ src/models/components/classification_models.py
@@ -3,19 +3,31 @@ import torch.nn as nn
 import torchvision
 from torchvision.models.resnet import ResNet50_Weights
 
+
 class ResNet50Module(nn.Module):
     """
     Binary classification model based on DeepLabv3.
     """
 
-    def __init__(self):
+    def __init__(self, nchannel = 3 ):
         super().__init__()
-        model = torchvision.models.resnet50(
+        self.model = torchvision.models.resnet50(
                     weights=ResNet50_Weights.DEFAULT
                 )
-        num_features = model.fc.in_features
-        nn.Linear(num_features, 2)
+
+        self.model.fc  = nn.Linear(in_features=2048, out_features=2, bias=True)
+
+        if nchannel != 3:
+            self.model.conv1 = nn.Conv2d(
+                nchannel,
+                64,
+                kernel_size=(7, 7),
+                stride=(2, 2),
+                padding=(3, 3),
+                bias=False,
+            )
 
 
     def forward(self, input):
         return input.view(input.size(0), -1)
+
