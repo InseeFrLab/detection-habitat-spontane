3f9859eb73f9cc9aa2a0b442f0e7b2a48c604cb4
tseimandi <tom.seimandi@insee.fr>
2023-07-27 08:59:06 +0000

    Add non-tested boilerplate code for anchor generation tuning

--- src/models/components/detection_models.py
+++ src/models/components/detection_models.py
@@ -23,6 +23,18 @@ class FasterRCNNModule(nn.Module):
         # replace the pre-trained head with a new one
         self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
 
+        """
+        # Anchor generation
+        anchor_generator = AnchorGenerator(
+            sizes=((10,), (20,), (30,), (40,), (50,), (60,))
+            aspect_ratios=tuple([(0.25, 0.5, 1.0, 2.0) for _ in range(5)])
+        )
+        model.rpn.anchor_generator = anchor_generator
+
+        # 256 because that's the number of features that FPN returns
+        model.rpn.head = RPNHead(256, anchor_generator.num_anchors_per_location()[0])
+        """
+
     def forward(self, x):
         """
         Forward pass.
