dc1abecc9153bb39844f6c7ab3e500450b7a5800
tseimandi <tom.seimandi@insee.fr>
2023-10-12 13:46:38 +0000

    Test segmentation run

--- bash/mlflow_train_segmentation.sh
+++ bash/mlflow_train_segmentation.sh
@@ -3,27 +3,11 @@
 # Set MLFLOW_EXPERIMENT_NAME environment variable
 export MLFLOW_EXPERIMENT_NAME="segmentation"
 
-# Set MLFLOW_RUN_NAME environment variable
-export MLFLOW_RUN_NAME="testraya_1"
-
 # Set MLFLOW_EXPERIMENT_NAME environment variable
 export MLFLOW_S3_ENDPOINT_URL='https://minio.lab.sspcloud.fr'
+export MLFLOW_TRACKING_URI="https://projet-slums-detection-128833.user.lab.sspcloud.fr"
 
-# Set MLFLOW_TRACKING_URI environment variable
-GET_PODS=`kubectl get pods`
-
-while IFS= read -r line; do
-    VAR=`echo "${line}" | sed -n "s/.*mlflow-\([0-9]\+\)-.*/\1/p"`
-    if [ -z "$VAR" ]; then
-        :
-    else
-        POD_ID=$VAR
-    fi
-done <<< "$GET_PODS"
-
-export MLFLOW_TRACKING_URI="https://projet-slums-detection-$POD_ID.user.lab.sspcloud.fr"
-
-mlflow run ~/work/detection-bidonvilles/ --entry-point segmentation --env-manager=local \
+mlflow run ~/work/detection-habitat-spontane/ --entry-point segmentation --env-manager=local \
 -P remote_server_uri=$MLFLOW_TRACKING_URI \
 -P experiment_name=$MLFLOW_EXPERIMENT_NAME \
 -P run_name=$MLFLOW_RUN_NAME
