db3272082c1e7aa5b4e799308e13d202e7e2a7d2
rayaberova7 <rayaberova7@gmail.com>
2023-10-24 10:10:53 +0000

    modele DTPNet implémenté

--- src/models/components/segmentation_models.py
+++ src/models/components/segmentation_models.py
@@ -39,61 +39,73 @@ class DeepLabv3Module(nn.Module):
 
 class DTPNet(nn.Module):
     def __init__(self):
-        super(DTPNet, self).__init()
+        super(DTPNet, self).__init__()
 
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=1, padding='same')
-        self.conv2 = nn.Conv2d(in_channels=8, out_channels=8, kernel_size=(3, 3), stride=1, padding='same')
-        self.bn1 = nn.BatchNorm2d(num_features=8, affine=True)
-        self.relu1 = nn.ReLU()
-        self.pool1 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
+        self.conv_layers1 = nn.ModuleList([
+            nn.Conv2d(in_channels=1, out_channels=8, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.Conv2d(in_channels=8, out_channels=8, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.BatchNorm2d(num_features=8, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
+        ])
+        
+        self.conv_layers2 = nn.ModuleList([
+            nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.BatchNorm2d(num_features=16, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
+        ])
 
-        self.conv3 = nn.Conv2d(in_channels=8, out_channels=16, kernel_size=(3, 3), stride=1, padding='same')
-        self.conv4 = nn.Conv2d(in_channels=16, out_channels=16, kernel_size=(3, 3), stride=1, padding='same')
-        self.bn2 = nn.BatchNorm2d(num_features=16, affine=True)
-        self.relu2 = nn.ReLU()
-        self.pool2 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
+        self.conv_layers3 = nn.ModuleList([
+            nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.BatchNorm2d(num_features=32, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
+        ])
+        
+        self.conv_layers4 = nn.ModuleList([
+            nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.BatchNorm2d(num_features=64, affine=True),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
+        ])
 
-        self.conv5 = nn.Conv2d(in_channels=16, out_channels=32, kernel_size=(3, 3), stride=1, padding='same')
-        self.conv6 = nn.Conv2d(in_channels=32, out_channels=32, kernel_size=(3, 3), stride=1, padding='same')
-        self.bn3 = nn.BatchNorm2d(num_features=32, affine=True)
-        self.relu3 = nn.ReLU()
-        self.pool3 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
+        self.conv_layers5 = nn.ModuleList([
+            nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.BatchNorm2d(num_features=128, affine=True),
+            nn.ReLU()
+        ])
 
-        self.conv7 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=(3, 3), stride=1, padding='same')
-        self.conv8 = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=(3, 3), stride=1, padding='same')
-        self.bn4 = nn.BatchNorm2d(num_features=64, affine=True)
-        self.relu4 = nn.ReLU()
-        self.pool4 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
-
-        self.conv9 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=(3, 3), stride=1, padding='same')
-        self.conv10 = nn.Conv2d(in_channels=128, out_channels=128, kernel_size=(3, 3), stride=1, padding='same')
-        self.bn5 = nn.BatchNorm2d(num_features=128, affine=True)
-        self.relu5 = nn.ReLU()
-        self.pool5 = nn.MaxPool2d(kernel_size=(2, 2), stride=2, padding=0)
-
-        self.conv11 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1, padding='same')
-        self.bn6 = nn.BatchNorm2d(num_features=256, affine=True)
-        self.relu6 = nn.ReLU()
+        self.conv_layers6 = nn.ModuleList([
+            nn.Conv2d(in_channels=128, out_channels=256, kernel_size=(3, 3), stride=1, padding='same'),
+            nn.BatchNorm2d(num_features=256, affine=True),
+            nn.ReLU()
+        ])
 
         self.dropout1 = nn.Dropout2d(p=0.2)
-        self.fc = nn.Linear(256, 1)
+        self.fc = nn.Linear(14*14*256, 1)
         self.regression = nn.Linear(1, 1) 
 
         
     def forward(self, x):
-        x = self.pool1(self.relu1(self.bn1(self.conv2(self.conv1(x)))))
+        for layer in self.conv_layers1:
+            x = layer(x)
+        for layer in self.conv_layers2:
+            x = layer(x)
+        for layer in self.conv_layers3:
+            x = layer(x)
+        for layer in self.conv_layers4:
+            x = layer(x)
+        for layer in self.conv_layers5:
+            x = layer(x)
+        for layer in self.conv_layers6:
+            x = layer(x)
 
-        x = self.pool2(self.relu2(self.bn2(self.conv4(self.conv3(x)))))
-
-        x = self.pool3(self.relu3(self.bn3(self.conv6(self.conv5(x)))))
-
-        x = self.pool4(self.relu4(self.bn4(self.conv8(self.conv7(x)))))
-
-        x = self.pool5(self.relu5(self.bn5(self.conv10(self.conv9(x)))))
-
-        x = self.relu6(self.bn6(self.conv11(x)))
-
-        x = self.dropout(x)
+        x = self.dropout1(x)
 
         # Applatissement des données pour la FC
         x = x.view(x.size(0), -1)
